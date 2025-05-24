9caf8aef11d4897f6d2b18853526a48f9a1a51b5
Lucas Martins Domingues <lucasmdomingues.dev@gmail.com>
2025-05-24 20:10:05 -0300

    Fix test

--- math_test.go
+++ math_test.go
@@ -1,12 +1,9 @@
 package main
 
-import (
-	"math/rand/v2"
-	"testing"
-)
+import "testing"
 
 func TestSum(t *testing.T) {
-	want := rand.IntN(100)
+	want := 30
 	got := Sum(15, 15)
 	if got != want {
 		t.Errorf("Invalid sum, Got %d. Want %d", got, want)
