542042660d25359322260ebdefdfcb060616d2a5
Lucas Martins Domingues <lucasmdomingues.dev@gmail.com>
2025-05-24 20:08:12 -0300

    added value to break CI

--- math_test.go
+++ math_test.go
@@ -1,9 +1,12 @@
 package main
 
-import "testing"
+import (
+	"math/rand/v2"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	want := 30
+	want := rand.IntN(100)
 	got := Sum(15, 15)
 	if got != want {
 		t.Errorf("Invalid sum, Got %d. Want %d", got, want)
